85bbefb06631fa68f9acfe4cac1c46af52abd7c2
Marcelo Borges <marcelosbgs@gmail.com>
2025-06-13 06:13:49 -0300

    interface start

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Chatbot PyGPT",
+    page_icon=":robot_face:",
+)
+
+st.header('Chatbot com seus documentos (RAG)')
+
+st.write("""
+""")
