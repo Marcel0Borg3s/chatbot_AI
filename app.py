1813fdf4facdc98519c4239d34323ee7183fa880
Marcelo Borges <marcelosbgs@gmail.com>
2025-06-14 13:16:47 -0300

    interface streamlit

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
+import os
 import streamlit as st
+
+from decouple import config
+
+os.environ['OPENAI_API_KEY'] = config('OPENAI_API_KEY')
 
 st.set_page_config(
     page_title="Chatbot PyGPT",
@@ -7,5 +12,35 @@ st.set_page_config(
 
 st.header('Chatbot com seus documentos (RAG)')
 
-st.write("""
-""")
+with st.sidebar:
+    st.header('Upload de arquivos')
+    uploaded_files = st.file_uploader(
+        label='Upload dos arquivos PDF',
+        type=['pdf'],
+        accept_multiple_files=True,
+        help='Selecione os arquivos PDF que deseja carregar:',
+    )
+
+    model_options = [
+        'gpt-3.5-turbo',
+        'gpt-4',
+        'gpt-4o',       
+        'gpt-4o-mini',
+        'gpt-4-turbo', 
+    ]
+
+    selected_model = st.sidebar.selectbox(
+        label='Selecione o modelo LLLM',
+        options=model_options,
+        index=0,
+        help='Selecione qual modelo de LLM você deseja usar:',
+    )
+
+    st.divider()
+    st.sidebar.markdown('###### Criado por:')
+    st.sidebar.markdown('###### [@marcelo_borges](https://www.linkedin.com/in/mborgesx/)')
+    
+
+question = st.chat_input('Digite sua pergunta aqui:')
+
+st.chat_message('user').write(question)
